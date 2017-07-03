504a9729656ea8a7891ab4ed3301dee20da2e1b1
Wei Tang <hi@that.world>
2017-07-04 01:13:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# blockchain-rs
+
+A generic blockchain interface for Rust, with native interface support. Works similar as Tendermint's framework.
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub enum Message {
+    DeliverTx,
+    CheckTx,
+    Commit
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
