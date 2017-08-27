ad407f3bbb317e043c7f189c465509a5413038fd
Wei Tang <hi@that.world>
2017-08-27 21:41:29 +0800

    Add a default implementation for Chain

--- src/chain.rs
+++ src/chain.rs
@@ -1,4 +1,6 @@
 use std::marker::PhantomData;
+use std::collections::HashMap;
+use std::hash::Hash;
 
 pub trait HeaderHash<H: Copy> {
     fn parent_hash(&self) -> H;
@@ -9,8 +11,21 @@ pub trait HeaderStore {
     type Hash: Copy;
     type Header: HeaderHash<Self::Hash> + Ord;
 
-    fn get(&self, hash: Self::Hash) -> &Self::Header;
-    fn insert(&mut self, block: Self::Header);
+    fn fetch(&self, hash: Self::Hash) -> Option<&Self::Header>;
+    fn put(&mut self, block: Self::Header);
+}
+
+impl<Ha: Copy + Eq + Hash, He: HeaderHash<Ha> + Ord> HeaderStore for HashMap<Ha, He> {
+    type Hash = Ha;
+    type Header = He;
+
+    fn fetch(&self, hash: Ha) -> Option<&He> {
+        self.get(&hash)
+    }
+
+    fn put(&mut self, header: He) {
+        self.insert(header.header_hash(), header);
+    }
 }
 
 pub struct Chain<H, B, S> {
@@ -22,14 +37,14 @@ pub struct Chain<H, B, S> {
 impl<H: Copy, B: HeaderHash<H> + Ord, S: HeaderStore<Hash=H, Header=B>> Chain<H, B, S> {
     pub fn best(&self) -> &B {
         let best_hash = self.best_hash;
-        self.get(best_hash)
+        self.fetch(best_hash).unwrap()
     }
 
-    pub fn get(&self, hash: H) -> &B {
-        self.store.get(hash)
+    pub fn fetch(&self, hash: H) -> Option<&B> {
+        self.store.fetch(hash)
     }
 
-    pub fn insert(&mut self, block: B) {
+    pub fn put(&mut self, block: B) {
         let extern_hash = block.header_hash();
         let local_hash = self.best_hash;
         let best_hash = if &block > self.best() {
@@ -37,7 +52,7 @@ impl<H: Copy, B: HeaderHash<H> + Ord, S: HeaderStore<Hash=H, Header=B>> Chain<H,
         } else {
             local_hash
         };
-        self.store.insert(block);
+        self.store.put(block);
         self.best_hash = best_hash;
     }
 }
